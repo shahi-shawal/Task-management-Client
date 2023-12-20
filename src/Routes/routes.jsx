2c477473af6e44e55699115b806e9d790df2739b
Shawal <shahishawal@gmail.com>
2023-12-20 23:24:33 +0600

    Routes implement

---
+++ src/Routes/routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const routes = createBrowserRouter([{
+    path:"/",
+    element:<h1>Hi Task Manager</h1>
+}])
+
+export default routes;
